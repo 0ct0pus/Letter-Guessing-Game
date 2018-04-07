daa028d5d85f88820ca7dbeaa58c4998179655ef
Max <31572255+0ct0pus@users.noreply.github.com>
2018-04-07 21:03:31 +0800

    Add files via upload

---
+++ JS/game1.js
@@ -0,0 +1,29 @@
+console.log("game1.js initialised.")
+
+//jQuery
+$(document).ready(() => {
+  const $aButton = $('.a-button');
+  const $oButton = $('.o-button');
+  const $uButton = $('.u-button');
+
+  $aButton.on(('click'), () => {
+    $aButton.addClass('button-active-correct');
+    $('.catText').replaceWith("<h1 class='catText' align='center'>Cat</h1>");
+    $('.catText2').replaceWith("<h1 class='catText' align='center'>Cat</h1>");
+
+  })
+
+  $oButton.on(('click'), () => {
+    $oButton.addClass('button-active-incorrect');
+    $('.catText').replaceWith("<h1 class='catText2' align='center'>Cot</h1>");
+    $('.catText2').replaceWith("<h1 class='catText2' align='center'>Cot</h1>");
+  })
+
+  $uButton.on(('click'), () => {
+    $uButton.addClass('button-active-incorrect');
+    $('.catText').replaceWith("<h1 class='catText2' align='center'>Cut</h1>");
+    $('.catText2').replaceWith("<h1 class='catText2' align='center'>Cut</h1>");
+  })
+
+
+})
